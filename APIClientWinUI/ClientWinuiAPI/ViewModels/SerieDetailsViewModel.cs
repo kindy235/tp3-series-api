6e37de654bfe3289b71f75821ca5a5f7c38530c5
kindy235 <bahaboubacar03@gmail.com>
2023-11-04 00:56:00 +0100

    Partie 4 : add notatio to serie OK

--- APIClientWinUI/ClientWinuiAPI/ViewModels/SerieDetailsViewModel.cs
+++ APIClientWinUI/ClientWinuiAPI/ViewModels/SerieDetailsViewModel.cs
@@ -1,17 +1,23 @@
 ﻿using ClientWinuiAPI.Models;
+using ClientWinuiAPI.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using static com.sun.tools.javap.TypeAnnotationWriter;
 
 namespace ClientWinuiAPI.ViewModels;
 
 public partial class SerieDetailsViewModel : ObservableRecipient
 {
     public Serie? serie;
+    //private readonly UserService _userService;
     public SerieDetailsViewModel()
     {
         Serie = new Serie();
         Serie = serie;
+        //_userService = UserService.GetService;
+        listSerieNotes = new ObservableCollection<Notation>();
         AddNotationtoSerie = new RelayCommand(PerformAddNotationtoSerie);
 
     }
@@ -24,11 +30,18 @@ public partial class SerieDetailsViewModel : ObservableRecipient
             if (serie != value)
             {
                 serie = value;
+                if (serie?.NotesSerie !=null)
+                {
+                    foreach (var notation in serie.NotesSerie)
+                    {
+                        listSerieNotes.Add(notation);
+                    }
+                    ListSerieNotes = listSerieNotes;
+                }
                 OnPropertyChanged(nameof(serie));
             }
         }
     }
-
     public ICommand AddNotationtoSerie
     {
         get; set;
@@ -36,13 +49,45 @@ public partial class SerieDetailsViewModel : ObservableRecipient
 
     private void PerformAddNotationtoSerie()
     {
+        var note = NotationValue;
+
+        var notation = new Notation {
+            UtilisateurId = note%2,
+            SerieId = Serie.SerieId,
+            Note = note,
+        };
+        listSerieNotes.Add(notation);
+        Serie.NotesSerie.Add(notation);
+        OnPropertyChanged(nameof(Serie));
+        ListSerieNotes = listSerieNotes;
     }
 
-    private string notationValue;
 
-    public string NotationValue
+    private ObservableCollection<Notation> listSerieNotes;
+    public ObservableCollection<Notation> ListSerieNotes
+    {
+        get => listSerieNotes;
+        set
+        {
+            if (listSerieNotes != value)
+            {
+                listSerieNotes = value;
+                OnPropertyChanged(nameof(listSerieNotes));
+            }
+        }
+    }
+
+    private int notationValue;
+    public int NotationValue
     {
         get => notationValue;
-        set => SetProperty(ref notationValue, value);
+        set
+        {
+            if (notationValue != value)
+            {
+                notationValue = value;
+                OnPropertyChanged(nameof(notationValue));
+            }
+        }
     }
 }
